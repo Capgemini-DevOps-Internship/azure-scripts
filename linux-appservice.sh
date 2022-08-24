d976a7eb4f1f6b3cfdeb0c65bb0a111f1d2498ff
Herbert-Moore <110556492+Herbert-Moore@users.noreply.github.com>
2022-08-24 15:38:30 +0200

    Update linux-appservice.sh

--- linux-appservice.sh
+++ linux-appservice.sh
@@ -2,4 +2,4 @@
 
 az appservice plan create -n linux-appservice-plan -g build-agents-cli-westeurope -l westeurope --is-linux --number-of-workers 1 --sku S1
 
-az webapp create -n capgemlinuxwebapp -g build-agents-cli-westeurope -p linux-appservice-plan -i herbertmoore/static-website-01:hello_world -s herbertmoore -w ATsSvK32T95b
+az webapp create -n capgemlinuxwebapp -g build-agents-cli-westeurope -p linux-appservice-plan -i herbertmoore/static-website-01:hello_world
