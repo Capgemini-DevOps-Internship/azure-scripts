e7b1a3a88c3949476a4cd7a1f6c93d825c775a46
Herbert-Moore <110556492+Herbert-Moore@users.noreply.github.com>
2022-08-25 10:30:46 +0200

    Update create-vm.sh

--- create-vm.sh
+++ create-vm.sh
@@ -2,12 +2,12 @@
 
 az disk create -g build-agents-cli-westeurope -n agent-disk --tier P4 -z 30
 
-az vm create -g build-agents-cli-westeurope -n agent --image UbuntuLTS --vnet-name build-agents-vnet --subnet agents-subnet --size Standard_DS1_v2 --public-ip-address-allocation dynamic --public-ip-address-dns-name capgemagent 
+az vm create -g build-agents-cli-westeurope -n agent --image UbuntuLTS --vnet-name build-agents-vnet --subnet agents-subnet --size Standard_DS1_v2 --public-ip-address-allocation dynamic --public-ip-address-dns-name capgemagent --generate-ssh-keys
 
 az vm disk attach --vm-name agent -n agent-disk
 
 az disk create -g build-agents-cli-westeurope -n jumpbox-disk --tier P10 -z 127
 
-az vm create -g build-agents-cli-westeurope -n jumpbox --image Win2022Datacenter --vnet-name build-agents-vnet --subnet jumpbox-subnet --size Standard_DS1_v2 --public-ip-address-allocation static 
+az vm create -g build-agents-cli-westeurope -n jumpbox --image Win2022Datacenter --vnet-name build-agents-vnet --subnet jumpbox-subnet --size Standard_DS1_v2 --public-ip-address-allocation static --authentication-type ssh --generate-ssh-keys
         
 az vm disk attach --vm-name jumpbox -n jumpbox-disk
