81f7cf54faf2b50662bd676bbfbe7a1f55b0ab9f
Herbert-Moore <110556492+Herbert-Moore@users.noreply.github.com>
2022-08-25 17:14:46 +0200

    Update create-vm.sh

--- create-vm.sh
+++ create-vm.sh
@@ -2,12 +2,12 @@
 
 az disk create -g build-agents-cli-westeurope -n agent-disk --tier P4 -z 30
 
-az vm create -g build-agents-cli-westeurope -n agent --image UbuntuLTS --vnet-name build-agents-vnet --subnet agents-subnet --size Standard_DS1_v2 --data-disk-delete-option delete --nic-delete-option delete --public-ip-address-allocation dynamic --public-ip-address-dns-name capgemagent --admin-username azureuser --generate-ssh-keys
+az vm create -g build-agents-cli-westeurope -n agent --image UbuntuLTS --vnet-name build-agents-vnet --subnet agents-subnet --size Standard_DS1_v2 --os-disk-delete-option delete --data-disk-delete-option delete --nic-delete-option delete --public-ip-address-allocation dynamic --public-ip-address-dns-name capgemagent --admin-username azureuser --generate-ssh-keys
 
 az vm disk attach --vm-name agent -n agent-disk -g build-agents-cli-westeurope
 
 az disk create -g build-agents-cli-westeurope -n jumpbox-disk --tier P10 -z 127
 
-az vm create -g build-agents-cli-westeurope -n jumpbox --image Win2022Datacenter --vnet-name build-agents-vnet --subnet jumpbox-subnet --size Standard_DS1_v2 --data-disk-delete-option delete --nic-delete-option delete --public-ip-address-allocation static --admin-password Windowsp@55word
+az vm create -g build-agents-cli-westeurope -n jumpbox --image Win2022Datacenter --vnet-name build-agents-vnet --subnet jumpbox-subnet --size Standard_DS1_v2 --os-disk-delete-option delete --data-disk-delete-option delete --nic-delete-option delete --public-ip-address-allocation static --admin-password Windowsp@55word
         
 az vm disk attach --vm-name jumpbox -n jumpbox-disk -g build-agents-cli-westeurope
