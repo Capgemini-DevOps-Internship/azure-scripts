29d357ed43e6f52e0b81a8fba66f92223ebda47a
Herbert-Moore <110556492+Herbert-Moore@users.noreply.github.com>
2022-08-26 14:17:58 +0200

    Update create-vm.ps1

--- create-vm.ps1
+++ create-vm.ps1
@@ -5,12 +5,12 @@ $virtualmachine1 = New-AzVM -Name agent -ResourceGroupName build-agents-powershe
 $diskconfig1 = New-AzDiskConfig -Tier P4 -DiskSizeGB 30 -Location westeurope -CreateOption Empty -SkuName Premium_LRS
 $datadisk1 = New-AzDisk -DiskName agent-disk -Disk $diskconfig1 -ResourceGroupName build-agents-powershell-westeurope
 $vm1 = Get-AzVM -Name agent -ResourceGroupName build-agents-powershell-westeurope
-$vm1 = Add-AzVMDataDisk -VM $vm1 -Name agent-disk -CreateOption Attach -lun 1
-Update-AzVM -VM $vm1 -ResourceGroupName build-agents-powershell-westeurope  $datadisk1.managedDisk
+$vm1 = Add-AzVMDataDisk -VM $vm1 -Name agent-disk -CreateOption Attach -lun 1 -DeleteOption delete -ManagedDiskID $datadisk1.Id
+Update-AzVM -VM $vm1 -ResourceGroupName build-agents-powershell-westeurope 
 
 $virtualmachine2 = New-AzVM -Name jumpbox -ResourceGroupName build-agents-powershell-westeurope -Image Win2019Datacenter -Size Standard_DS1_v2 -VirtualNetworkName build-agents-vnet -SubnetName jumpbox-subnet -AllocationMethod Static -PublicIpAddressName reserved-ip -Credential $Credential
 $diskconfig2 = New-AzDiskConfig -Tier P10 -DiskSizeGB 127 -Location westeurope -CreateOption Empty -SkuName Premium_LRS
 $datadisk2 = New-AzDisk -DiskName jumpbox-disk -Disk $diskconfig2 -ResourceGroupName build-agents-powershell-westeurope 
 $vm2 = Get-AzVM -Name jumpbox -ResourceGroupName build-agents-powershell-westeurope
-$vm2 = Add-AzVMDataDisk -VM $vm2 -Name jumpbox-disk -CreateOption Attach -lun 1
-Update-AzVM -VM $vm2 -ResourceGroupName build-agents-powershell-westeurope -ManagedDiskId $datadisk2.Id
+$vm2 = Add-AzVMDataDisk -VM $vm2 -Name jumpbox-disk -CreateOption Attach -lun 1 -DeleteOption delete -ManagedDiskID $datadisk1.Id
+Update-AzVM -VM $vm2 -ResourceGroupName build-agents-powershell-westeurope
