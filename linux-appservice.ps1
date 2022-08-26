4ef365c744f9d3fc4075a824fd5fe405d4ba1cc6
Herbert-Moore <110556492+Herbert-Moore@users.noreply.github.com>
2022-08-26 16:05:45 +0200

    Update linux-appservice.ps1

--- linux-appservice.ps1
+++ linux-appservice.ps1
@@ -1,3 +1,4 @@
 New-AzAppServicePlan -ResourceGroupName "build-agents-powershell-westeurope" -Name "linux-webapp-plan" -Location "westeurope" -Tier "Standard" -NumberofWorkers 1 -Linux 
 
-New-AzWebApp -ResourceGroupName build-agents-powershell-westeurope -Name "capgem-linux-webapp" -Location "westeurope" -AppServicePlan "linux-webapp-plan" -ContainerImageName static-website-01:hello_world -ContainerRegistryURL https://hub.docker.com/repository/docker/herbertmoore/static-website-01 -ContainerRegistryUser ${env:DOCKER_HUB_USERNAME} -ContainerRegistryPassword ${env:DOCKER_HUB_PASSWORD}
+$azureSecuredPassword = ConvertTo-SecureString ${env:DOCKER_HUB_PASSWORD} -AsPlainText -Force
+New-AzWebApp -ResourceGroupName build-agents-powershell-westeurope -Name "capgem-linux-webapp" -Location "westeurope" -AppServicePlan "linux-webapp-plan" -ContainerImageName static-website-01:hello_world -ContainerRegistryURL https://hub.docker.com/repository/docker/herbertmoore/static-website-01 -ContainerRegistryUser ${env:DOCKER_HUB_USERNAME} -ContainerRegistryPassword $azureSecuredPassword
