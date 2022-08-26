cdbc9d0900fe86ab68278904945ec910bcb5ae83
Herbert-Moore <110556492+Herbert-Moore@users.noreply.github.com>
2022-08-26 17:15:11 +0200

    Update create-storage-account.ps1

--- create-storage-account.ps1
+++ create-storage-account.ps1
@@ -1,1 +1,1 @@
-New-AzStorageAccount -ResourceGroupName build-agents-powershell-westeurope -Name capgemstorageaccount -Location westeurope -AccessTier Hot
+New-AzStorageAccount -ResourceGroupName build-agents-powershell-westeurope -Name capgemstorageaccount -Location westeurope -AccessTier Hot -SkuName Premium_LRS
