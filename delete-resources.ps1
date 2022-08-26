cf3e4a31173b5a7e13f2cdd4ea153661061ab3b4
Herbert-Moore <110556492+Herbert-Moore@users.noreply.github.com>
2022-08-26 18:09:36 +0200

    Update delete-resources.ps1

--- delete-resources.ps1
+++ delete-resources.ps1
@@ -1,1 +1,13 @@
+$VirtualMachine1 = Get-AzVM -ResourceGroupName "build-agents-powershell-westeurope" -Name "agent"
+Remove-AzVMDataDisk -VM $VirtualMachine1 -Name "agent-disk"
+Update-AzVM -ResourceGroupName "build-agents-powershell-westeurope" -VM $VirtualMachine1
+
+$VirtualMachine2 = Get-AzVM -ResourceGroupName "build-agents-powershell-westeurope" -Name "jumpbox"
+Remove-AzVMDataDisk -VM $VirtualMachine2 -Name "jumpbox-disk"
+Update-AzVM -ResourceGroupName "build-agents-powershell-westeurope" -VM $VirtualMachine2
+
+$virtualnetwork = Get-AzVirtualNetwork -Name build-agents-vnet -ResourceGroupName build-agents-powershell-westeurope
+Set-AzVirtualNetworkSubnetConfig -Name agents-subnet -VirtualNetwork $virtualnetwork -AddressPrefix "10.0.0.0/24" -NetworkSecurityGroupId '""'
+Set-AzVirtualNetworkSubnetConfig -Name jumpbox-subnet -VirtualNetwork $virtualnetwork -AddressPrefix "10.0.1.0/24" -NetworkSecurityGroupId '""'
+
 Get-AzResource -ResourceGroupName build-agents-powershell-westeurope | Remove-AzResource -Force
