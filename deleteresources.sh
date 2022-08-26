dd0407b57d35a243294e92f4bd2a2942363ce6b1
Herbert-Moore <110556492+Herbert-Moore@users.noreply.github.com>
2022-08-26 09:56:52 +0200

    Update deleteresources.sh

--- deleteresources.sh
+++ deleteresources.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+az vm disk detach -g build-agents-cli-westeurope --vm-name agent --name agent-disk --force-detach
+az vm disk detach -g build-agents-cli-westeurope --vm-name jumpbox --name jumpbox-disk --force-detach
+
+az network vnet subnet update -g build-agents-cli-westeurope -n agents-subnet --vnet-name build-agents-vnet --network-security-group ""
+az network vnet subnet update -g build-agents-cli-westeurope -n jumpbox-subnet --vnet-name build-agents-vnet --network-security-group ""
+
 resources="$(az resource list --resource-group "build-agents-cli-westeurope" | grep id | awk -F \" '{print $4}')"
 
 for id in $resources; do
